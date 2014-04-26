ef7541dc2001f0e2c3cc3efb233f0aacd62c4386
Benjamin Hsieh <bhsiehh@gmail.com>
2014-04-25 17:40:09 -0700

    Add comments

--- lib/marvelous.rb
+++ lib/marvelous.rb
@@ -3,18 +3,23 @@ require 'marvelous/configuration'
 
 module Marvelous
   class << self
+    # Initialize Marvelous config
     def init
       yield(config)
+      config
     end
 
+    # Create or initialize configuration
     def config
       @config ||= Configuration.new
     end
   end
 
+  # Error class used to represent any top level Marvelous errors
   class MarvelousError < Exception
   end
 
+  # Error class used to represent any configuration errors
   class ConfigurationError < Exception
   end
 end
